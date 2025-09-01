866a61243125e319261a8f826a9bb5af1d5a66c4
Peter Radisson <Radisson97@web.de>
2025-09-01 18:07:56 +0200

    make static
    function dialog_close is now static

--- dialbox.h
+++ dialbox.h
@@ -37,6 +37,6 @@ void dialog_close(int return_value);
 #define DB_V_SPACE 2 /* Extra space above and below text */
 
 
-void dialog_close(int return_value) ;
+//void dialog_close(int return_value) ;
 
 #endif
