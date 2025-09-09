824b2faa6612e0a3d5b665dac4a31f6f818230c3
Peter Radisson <Radisson97@web.de>
2025-09-09 14:50:47 +0200

    more info
    add some more info

--- modules/power_line.c
+++ modules/power_line.c
@@ -22,9 +22,9 @@ do_power_line (int x, int y)
     if (grid[MP_INFO(x,y).int_6]->powered == -1)
 	return;
 
-    switch(MP_INFO(x,y).int_5) 
+    switch(MP_INFO(x,y).int_5)
     {
-    case 0: 
+    case 0:
 	MP_INFO(x,y).int_5 = POWER_MODULUS;
 	break;
     case 1:
@@ -45,10 +45,16 @@ do_power_line (int x, int y)
 void
 mps_power_line (int x, int y)
 {
-  int i = 0;
+	int i = 0;
+	int gridid;
+	mps_store_title(i++,_("Power Line"));
+	i++;
+	gridid=MP_INFO(x,y).int_6;
 
-  mps_store_title(i++,_("Power Line"));
-  i++;
+	mps_store_sd(i++,_("Grid ID"), gridid);
+	if (gridid <=0 ) return;
 
-  mps_store_sd(i++,_("Grid ID"),MP_INFO(x,y).int_6);
+	mps_store_sd(i++,_("demand"), grid[gridid]->demand);
+	mps_store_sd(i++,_("spare"),  grid[gridid]->total_power);
+
 }
